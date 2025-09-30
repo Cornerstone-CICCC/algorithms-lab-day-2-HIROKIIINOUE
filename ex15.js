d159d098bd5b33139b29354a82ffe25f051b524c
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 19:59:57 -0700

    done ex15.js

--- ex15.js
+++ ex15.js
@@ -1,5 +1,11 @@
-// Exercise 15: Write a recursive function `findMax` that takes an array of numbers 
+// Exercise 15: Write a recursive function `findMax` that takes an array of numbers
 // and returns the maximum number in the array.
 // Example: findMax([3, 1, 4, 1, 5, 9]) should return 9.
+
+const findMax = function (numbers) {
+  const descNumbers = numbers.sort((a, b) => b - a);
+  const maxNumber = descNumbers[0];
+  return maxNumber;
+};
 
 console.log(findMax([3, 1, 4, 1, 5, 9])); // Expected output: 9
