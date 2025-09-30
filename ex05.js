2199e2aec107923d97a5c50852434459c177a6d3
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 18:37:37 -0700

    done ex05.js

--- ex05.js
+++ ex05.js
@@ -2,4 +2,31 @@
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
 
+const countConsonants = function (sentence) {
+  let count = 0;
+  const length = sentence.length;
+  const sentenceArray = [];
+
+  for (let i = 0; i < length; i++) {
+    sentenceArray.push(sentence[i]);
+  }
+
+  sentenceArray.forEach((element) => {
+    if (
+      element === "a" ||
+      element === "i" ||
+      element === "u" ||
+      element === "e" ||
+      element === "o" ||
+      element === " "
+    ) {
+      return;
+    } else {
+      count++;
+    }
+  });
+
+  return count;
+};
+
 console.log(countConsonants("hello world")); // Expected output: 7
