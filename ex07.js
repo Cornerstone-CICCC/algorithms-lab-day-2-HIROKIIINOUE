783da929b5165bbbef1e82ce1e7af159596aae59
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 18:53:03 -0700

    done ex07.js

--- ex07.js
+++ ex07.js
@@ -3,5 +3,31 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
+const validateEmail = function (Email) {
+  if (!Email.includes("@")) {
+    return false;
+  }
+
+  const userName = Email.split("@")[0];
+  const domain = Email.split("@")[1];
+
+  if (!userName) {
+    return false;
+  }
+
+  if (!domain.includes(".")) {
+    return false;
+  }
+
+  const domainBeforeDot = domain.split(".")[0];
+  const domainAfterDot = domain.split(".")[1];
+
+  if (domainBeforeDot && domainAfterDot) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
