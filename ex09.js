731ac58a2e1c7d38b2e90e69302ed0e2f3732b66
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 19:26:16 -0700

    done ex09.js

--- ex09.js
+++ ex09.js
@@ -3,4 +3,23 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
-console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
+const sortByProperty = function (data, sortKey) {
+  const dataList = data;
+  if (sortKey === "name") {
+    dataList.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortKey === "age") {
+    dataList.sort((a, b) => a.age - b.age);
+  }
+
+  return dataList;
+};
+
+console.log(
+  sortByProperty(
+    [
+      { name: "Alice", age: 30 },
+      { name: "Bob", age: 25 },
+    ],
+    "age"
+  )
+); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
