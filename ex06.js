4b11b8345c8d3f3659cd7b1a7b76ccef7e1b592c
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 18:43:34 -0700

    done ex06.js

--- ex06.js
+++ ex06.js
@@ -3,4 +3,16 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+const getQueryParams = function (URL) {
+  const queryParametersList = URL.split("?")[1].split("&");
+  const result = {};
+
+  queryParametersList.forEach((element) => {
+    const key = element.split("=")[0];
+    const value = element.split("=")[1];
+    result[key] = value;
+  });
+  return result;
+};
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
