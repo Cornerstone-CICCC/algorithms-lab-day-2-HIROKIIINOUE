d9ac4879b9375aaa6d01b3ac750a4871ec5082b3
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 19:51:04 -0700

    done ex12.js

--- ex03.js
+++ ex03.js
@@ -2,4 +2,22 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+const findMissingNumber = function (array) {
+  const length = array.length;
+  const firstNumber = array[0];
+  const lastNumber = array[length - 1];
+  const idealArray = [];
+
+  let count = 1;
+
+  const result = array.filter((element) => {
+    if (element !== count) {
+      return count;
+    }
+    count++;
+  });
+
+  console.log(result);
+};
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
--- ex04.js
+++ ex04.js
@@ -2,4 +2,27 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+const mostFrequentChar = function (character) {
+  const length = character.length;
+  const array = [];
+  const object = {};
+  for (let i = 0; i < length; i++) {
+    array.push(character[i]);
+  }
+
+  array.forEach((element) => {
+    if (!object[element]) {
+      object[element] = 1;
+    } else {
+      object[element]++;
+    }
+  });
+
+  const newArray = [];
+
+  return object.length;
+  // const uniqueLetter = new Set(array);
+  // const uniqueLetterArray = [...uniqueLetter];
+};
+
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
