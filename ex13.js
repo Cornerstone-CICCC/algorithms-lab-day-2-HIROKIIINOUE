ea05c24e3fe5bf4fe526e047b88853fe85999fca
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-29 19:55:48 -0700

    done ex13.js

--- ex13.js
+++ ex13.js
@@ -2,4 +2,14 @@
 // and returns the sum of all numbers from 1 to `n`.
 // Example: sumRange(5) should return 15 (1 + 2 + 3 + 4 + 5).
 
+let count = 0;
+const sumRange = function (n) {
+  if (n > 0) {
+    count += n;
+    sumRange(n - 1);
+  }
+
+  return count;
+};
+
 console.log(sumRange(5)); // Expected output: 15
